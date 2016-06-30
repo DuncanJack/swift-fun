14a1c4ece6edf259662e5a30816294020bb925ae
= <=>
2016-06-30 19:25:27 +0100

    Collection Types - Dictionaries

--- CollectionTypes.playground/Contents.swift
+++ CollectionTypes.playground/Contents.swift
@@ -89,6 +89,61 @@ set3.isSuperset(of: set2)
 set3.isStrictSuperset(of: set2)
 set4.isDisjoint(with: set3)
 
+var set: NSSet = NSSet(array: [1,2,3])
+set.count
+
+// -------------------------------------------------------------------------------
+// Dictionaries
+// -------------------------------------------------------------------------------
+
+// Creating an Empty Dictionary
+
+var emptyDictionary = [Double:String]()
+
+
+// Creating a Dictionary with a Dictionary Literal
+
+var coffees = ["espresso":"*","americano":"**"]
+
+// Accessing and Modifying a Dictionary
+
+coffees.count
+coffees.isEmpty
+coffees["flat white"] = "**"
+coffees["flat white"] = "***"
+
+if let oldValue = coffees.updateValue("**", forKey: "espresso") {
+    print("The old value for espresso was \(oldValue)")
+}
+
+coffees["mocha"]
+
+coffees["chai"] = "*"
+coffees["chai"] = nil
+
+print(coffees)
+
+if let removedValue = coffees.removeValue(forKey: "flat white") {
+    print("removedValue: \(removedValue)")
+}
+
+// Iterating Over a Dictionary
+
+for (name, rating) in coffees {
+    print("\(name) \(rating)")
+}
+
+[String](coffees.keys)
+[String](coffees.values)
+
+if var americano = coffees["americano"] {
+    print("Would you like hot milk?")
+}
+
+var dictionary: NSDictionary = NSDictionary(dictionary: ["A":"a","B":"b"])
+dictionary["A"]
+
+
 
 
 
