6d6f0a11d6a4d6fda023dd15eb3c29fb1bbd6803
= <=>
2016-07-05 18:05:08 +0100

    Inheritance - started

--- Inheritance.playground/Contents.swift
+++ Inheritance.playground/Contents.swift
@@ -1,3 +1,42 @@
 // -------------------------------------------------------------------------------
 // Defining a Base Class
 // -------------------------------------------------------------------------------
+
+class Being {
+    func breathe() {
+        print("breathe")
+    }
+}
+
+let being = Being()
+being.breathe()
+
+// -------------------------------------------------------------------------------
+// Subclassing
+// -------------------------------------------------------------------------------
+
+class Human : Being {
+    func speak() {
+        print("speak")
+    }
+}
+
+let human = Human()
+human.breathe()
+human.speak()
+
+// -------------------------------------------------------------------------------
+// Overriding
+// -------------------------------------------------------------------------------
+
+// Overriding Methods
+
+class Male : Human {
+    override func speak() {
+        print("male speak")
+    }
+}
+
+let male = Male()
+male.breathe()
+male.speak()
