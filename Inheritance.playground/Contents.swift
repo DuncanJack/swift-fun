883aeef42955e7b234259ec48346688a90765b0b
= <=>
2016-07-05 18:58:47 +0100

    Inheritance - completed

--- Inheritance.playground/Contents.swift
+++ Inheritance.playground/Contents.swift
@@ -3,6 +3,7 @@
 // -------------------------------------------------------------------------------
 
 class Being {
+    
     func breathe() {
         print("breathe")
     }
@@ -16,6 +17,15 @@ being.breathe()
 // -------------------------------------------------------------------------------
 
 class Human : Being {
+    var description = "Being"
+    var weight:Int? {
+        didSet {
+            print("Human weight was \(oldValue ?? 0) and is \(weight ?? 0)")
+        }
+        willSet {
+            print("Human weight will be changed from \(weight ?? 0) to \(newValue ?? 0)")
+        }
+    }
     func speak() {
         print("speak")
     }
@@ -24,6 +34,7 @@ class Human : Being {
 let human = Human()
 human.breathe()
 human.speak()
+human.weight = 200
 
 // -------------------------------------------------------------------------------
 // Overriding
@@ -32,6 +43,22 @@ human.speak()
 // Overriding Methods
 
 class Male : Human {
+    override var description:String {
+        get {
+            return "Male.description.get"
+        }
+        set {
+            print("Male.description.set")
+        }
+    }
+    override var weight: Int? {
+        didSet {
+            print("Human weight was \(oldValue ?? 0) and is \(weight ?? 0)")
+        }
+        willSet {
+            print("Human weight will be changed from \(weight ?? 0) to \(newValue ?? 0)")
+        }
+    }
     override func speak() {
         print("male speak")
     }
@@ -40,3 +67,41 @@ class Male : Human {
 let male = Male()
 male.breathe()
 male.speak()
+
+// Overriding Properties
+
+male.weight = 190
+print(male.weight)
+print(male.description)
+
+// Overriding property observers
+
+class WeightWatcher : Human {
+    override var weight: Int? {
+        didSet {
+            print("WeightWatcher weight was \(oldValue ?? 0) and is \(weight ?? 0)")
+        }
+        willSet {
+            print("WeightWatcher weight will be changed from \(weight ?? 0) to \(newValue ?? 0)")
+        }
+    }
+}
+
+let weightWatcher = WeightWatcher()
+weightWatcher.weight = 195
+
+// -------------------------------------------------------------------------------
+// Preventing Overrides
+// -------------------------------------------------------------------------------
+
+final class Ultimate : Human {
+    override init() {
+        print("I cannot be overridden")
+    }
+}
+
+Ultimate()
+
+
+
+
