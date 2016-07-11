0891dd0467522615149bceb2b883ac4b84a30abc
= <=>
2016-07-11 19:35:43 +0100

    Automatic Reference Counting - Resolving Strong Reference Cycles Between Class Instances

--- AutomaticReferenceCounting.playground/Contents.swift
+++ AutomaticReferenceCounting.playground/Contents.swift
@@ -19,8 +19,10 @@ yourPerson = nil
 
 
 // -------------------------------------------------------------------------------
-// Strong Reference Cycles Between Class Instances
+// Resolving Strong Reference Cycles Between Class Instances
 // -------------------------------------------------------------------------------
+
+// Weak References
 
 class Man {
     var woman: Woman?
@@ -30,7 +32,7 @@ class Man {
 }
 
 class Woman {
-    var man: Man?
+    weak var man: Man?  // weak, now unowned, because a man may or may not have a woman
     deinit {
         print("woman.deinit")
     }
@@ -41,8 +43,73 @@ var woman: Woman? = Woman()
 man?.woman = woman
 woman?.man = man
 
-// Need this line to break the strong reference cycle
-man?.woman = nil
-
 man = nil
 woman = nil
+
+
+// Unowned Reference
+
+class Parent {
+    var child: Child?
+    deinit { print("parent.deinit") }
+}
+
+class Child {
+    unowned let parent: Parent  // unowned, not weak, because a child always has a parent
+    init(parent: Parent) {
+        self.parent = parent
+    }
+    deinit { print("child.deinit") }
+}
+
+var parent: Parent?
+var child: Child?
+
+parent = Parent()
+child = Child(parent:parent!)
+parent?.child = child
+
+parent = nil
+child = nil
+
+// Unowned References and Implicitly Unwrapped Optional Properties
+
+class Employer {
+    let name: String
+    var employee: Employee! // So Employer is fully initialized as soon as name is set, and self can be passed to Employee.
+    init(name: String, employeeName: String){
+        self.name = name
+        self.employee = Employee(name: employeeName, employer: self)
+    }
+}
+
+class Employee {
+    let name: String
+    unowned let employer: Employer
+    init(name: String, employer: Employer) {
+        self.name = name
+        self.employer = employer
+    }
+}
+
+var employer = Employer(name: "E", employeeName: "Phil")
+print("\(employer.name)'s employee is called \(employer.employee.name)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
