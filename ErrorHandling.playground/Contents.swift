4acbc4bd31e17d1d3a50473bc48eebcd2b3dd944
= <=>
2016-07-26 06:47:16 +0100

    Error Handling - Handling Errors (cont'd)

--- ErrorHandling.playground/Contents.swift
+++ ErrorHandling.playground/Contents.swift
@@ -14,3 +14,42 @@ enum NotAvailablerror: ErrorProtocol {
 // -------------------------------------------------------------------------------
 
 // Propogating Errors Using Throwing Functions
+
+enum CalculatorError: ErrorProtocol {
+    case Over10
+    case SomethingElse
+}
+
+class Calculator {
+    func add(a:Int, b:Int) throws -> Int {
+        guard a + b <= 10 else {
+            throw CalculatorError.Over10
+        }
+        guard a != 0 && b != 0 else {
+            throw CalculatorError.SomethingElse
+        }
+        return a + b
+    }
+}
+
+func add(a:Int, b:Int) throws -> Int {
+    return try Calculator().add(a: a, b: b)
+}
+
+print(try add(a:4,b:5))
+
+// Handling Errors Using Do-Catch
+
+do {
+    try add(a:6, b:6)
+} catch CalculatorError.Over10 {
+    print("The answer cannot be over 10.")
+}
+
+do {
+    try add(a:0, b:6)
+} catch {
+    print(error)
+}
+
+// Converting Errors to Optional Values
